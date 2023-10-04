036b5d28b5a64ac5f7c45eeb5bb8514820ba0da1
Sristi Shreya <shreyasristi@gmail.com>
2023-10-04 20:35:18 +0530

    Sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,19 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  })
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -70,10 +76,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId).then(()=>{
-    res.redirect('/admin/products');
-  }).
-  catch(err => {
-    console.error(err)
-  });
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
