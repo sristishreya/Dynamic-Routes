c6647f4e11b626f2f445bfdbe99cfed2aa7391d3
Sristi Shreya <shreyasristi@gmail.com>
2023-10-06 02:41:53 +0530

    Cart Items

--- controllers/admin.js
+++ controllers/admin.js
@@ -90,7 +90,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId)
+  Product.findByPk(prodId)
     .then(product => {
       return product.destroy();
     })
